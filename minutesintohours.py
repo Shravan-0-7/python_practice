916655ba67482d8431514078e2298234af4c5ff3
Shravan-0-7 <trynafind.shravan@gmail.com>
2026-09-10 15:20:46 +0530

    adding minutes into hours

--- minutesintohours.py
+++ minutesintohours.py
@@ -2,5 +2,5 @@ a=int(input("enter the minutes"))
 hours = a // 60
 remaining_minutes = a % 60
 
-print(a,"minutes is: ",hours,":",remaining_minutes,"minutes")
+print(a,"minutes is: ",hours," hours",remaining_minutes,"minutes")
 
