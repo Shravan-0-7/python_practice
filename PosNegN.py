76e4226ee2edddbd821cb485c7b4ceff04ef2b04
Shravan-0-7 <trynafind.shravan@gmail.com>
2026-09-10 23:10:29 +0530

    pos or neg until user quits

---
+++ PosNegN.py
@@ -0,0 +1,12 @@
+
+while(True):
+    x=input("enter the number or Quit: ")
+    if(x== "Quit"):
+        break
+    else:
+        num = int(x)
+        if(num >0 ):
+            print("num is positive")
+        else:
+            print("Num is negative")
+    
