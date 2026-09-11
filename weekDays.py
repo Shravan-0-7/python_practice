ca7fb5d301d1115f837d210202b0ea610c834cb7
Shravan-0-7 <trynafind.shravan@gmail.com>
2026-09-11 15:28:11 +0530

    number to week

---
+++ weekDays.py
@@ -0,0 +1,16 @@
+day = int(input("enter any number between 1-7: "))
+
+if day==0:
+    print("Sunday")
+elif day ==1:
+    print("Monday")
+elif day ==2:
+    print("Tuesday")
+elif day ==3:
+    print("Wednsday")
+elif day ==4:
+    print("Thursday")
+elif day ==5:
+    print("Friday")
+else:
+    print("saturday")
